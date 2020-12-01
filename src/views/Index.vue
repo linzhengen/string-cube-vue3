4e9bd93604e2d8ae35227b7b285c65312f88e444
linzhengen <linzhengen@yahoo.co.jp>
2020-12-02 01:27:58 +0900

    filter double

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,15 +3,27 @@
     <div class="container has-text-centered">
       <div class="columns">
         <div class="column is-5">
-          <textarea class="textarea is-focused" placeholder="入力してください" style="height: 100vh;">
+          <textarea
+            class="textarea is-focused"
+            placeholder="入力してください"
+            style="height: 100vh;"
+            v-model="data"
+          >
           </textarea>
         </div>
         <div class="column is-2">
-          <button class="button is-large">変換 <i class="fas fa-arrow-right"></i>
+          <button class="button is-large" @click="convert">
+            変換 <i class="fas fa-arrow-right"></i>
           </button>
         </div>
         <div class="column is-5">
-          <textarea class="textarea" placeholder="" readonly style="height: 100vh;"></textarea>
+          <textarea
+            class="textarea"
+            placeholder=""
+            readonly
+            style="height: 100vh;"
+            v-model="convertedData"
+          ></textarea>
         </div>
       </div>
     </div>
@@ -19,11 +31,24 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { defineComponent, ref } from 'vue';
+import _ from 'lodash';
 
-@Options({
-  components: {
+export default defineComponent({
+  name: 'Index',
+  setup() {
+    const data = ref<string>('');
+    const convertedData = ref<string>('');
+    return {
+      data,
+      convertedData,
+    };
   },
-})
-export default class Index extends Vue {}
+  methods: {
+    convert() {
+      const list = this.data.split('\n').filter((v) => v.trim().length > 0);
+      this.convertedData = _.uniq(list).join('\n');
+    },
+  },
+});
 </script>
